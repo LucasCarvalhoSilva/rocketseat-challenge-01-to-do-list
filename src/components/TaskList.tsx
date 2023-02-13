95d164535dd496895383791469472e3350bd9709
Lucas Carvalho Silva <lucascarvalhosilva2901@gmail.com>
2023-02-13 18:01:18 -0300

    Counters automatizeted

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,31 +1,41 @@
 
 import {ClipboardText} from 'phosphor-react'
+import { useState } from 'react';
 import { Task } from './Task';
 
 import styles from './TaskList.module.css'
 
 interface Task{
-    isCompleted?: boolean;
-    description?: string;
+    isCompleted: boolean;
+    description: string;
 }
 
 interface tasksProsp {
-    tasks?:Task[]
+    tasks:Task[]
 }
 
-export function TaskList(tasksList:tasksProsp) {
+export function TaskList({tasks}:tasksProsp) {
+    const [quantityOfTask,setQuantityOfTask] = useState(tasks.length) 
+    let countOfQuantityOfCompletedTasks = 0
     
-    if(tasksList.tasks !== null) {
+    tasks.forEach(task => {
+        if(task.isCompleted) {
+            countOfQuantityOfCompletedTasks++
+        }
+    })
+    const [quantityOfCompletedTasks, setQuantityOfCompletedTask] = useState(countOfQuantityOfCompletedTasks)
+
+    if(tasks !== null) {
         return(
             <div className={styles.container}>
                 <header className={styles.header}>
                     <div className={styles.createdTaskInfos}>
                         <p>Tarefas Criadas</p>
-                        <span className={styles.quantityOfTask}>0</span>
+                        <span className={styles.quantityOfTask}>{quantityOfTask}</span>
                     </div>
                     <div className={styles.completedTaskInfos}>
                         <p>Concluídas</p>
-                        <span className={styles.quantityOfCompletedTasks}>{0} de {5}</span>
+                        <span className={styles.quantityOfCompletedTasks}>{0} de {quantityOfTask}</span>
                     </div>
                 </header>
                 <Task />
